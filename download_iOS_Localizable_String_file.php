0e28d974a09f5d70f628fee8c9f528f87ebaddba
Limbasiya, Nirav <Nirav.Limbasiya@Honeywell.com>
2018-11-02 18:02:08 +0530

    Added zip download functionality

--- download_iOS_Localizable_String_file.php
+++ download_iOS_Localizable_String_file.php
@@ -9,23 +9,95 @@
         $selected_language = $_GET["SelectedLanguage"];
     }
     
-    header("Content-type: application/octet-stream");
-    header("Content-Disposition: attachment; filename=".$selected_language.".strings");
-    header("Pragma: no-cache");
-    header("Expires: 0");
-    // header('Content-Length: '.filesize('file.txt'));
+    // header("Content-type: application/octet-stream");
+    // header("Content-Disposition: attachment; filename=".$selected_language.".strings");
+    // header("Pragma: no-cache");
+    // header("Expires: 0");
+    // // header('Content-Length: '.filesize('file.txt'));
+
+    $file_name = './Localization/'.'Localizable.strings';
 
     $search_query = "SELECT StringKeys, English, `$selected_language` FROM $selected_card WHERE `$selected_language` != '' ";
 
-    $file = fopen('php://output', 'w');
+    if (!file_exists('./Localization')) {
+        mkdir('./Localization', 0777, true);
+    }
+    
+    $file = fopen($file_name, 'w') or die('Cannot open file:  '.$file_name);
 
-    if($search_result = mysqli_query($connection, $search_query) and mysqli_num_rows($search_result) > 0) {
-        while($row = mysqli_fetch_assoc($search_result))
-        {
-            fwrite($file, "\"".$row['StringKeys']."\" = \"".$row[$selected_language]."\";\r\n");
+    if ($file != NULL) {
+        if($search_result = mysqli_query($connection, $search_query) and mysqli_num_rows($search_result) > 0) {
+            while($row = mysqli_fetch_assoc($search_result))
+            {
+                fwrite($file, "\"".$row['StringKeys']."\" = \"".$row[$selected_language]."\";\r\n");
+            }
         }
+
+        fclose($file);
+
+        $zip = new ZipArchive;
+        $tmp_file = $selected_card.".zip";
+
+        if ($zip->open($tmp_file,  ZipArchive::CREATE | ZipArchive::OVERWRITE)) {
+            //Second argument is path where you want to put file inside zip when it unzip as folder
+            $zip->addFile($file_name, "/en.lproj/Localizable.strings");
+            $zip->close();
+
+            // echo 'Archive created! filename='.$tmp_file;
+            // echo filesize($tmp_file);
+
+            # send the file to the browser as a download
+            if (file_exists($tmp_file)) {
+                //Clear all header
+                while (ob_get_level()) {
+                    ob_end_clean();
+                }
+                
+                header('Content-Type: application/zip');
+                header('Content-Disposition: attachment; filename="'.basename($tmp_file).'"');
+                header('Content-Length: '. filesize($tmp_file));
+                readfile("./".$tmp_file);
+            }
+        } else {
+            echo 'Failed creating zip!';
+        }
+    } else {
+        echo 'Failed!';
     }
 
-    fclose($file);
+    unlink($file_name);
+    unlink($tmp_file);
+
     exit();
+?>
+
+<!-- working functionality to download Localizable.strings. -->
+<?php
+    // if(isset($_GET["SelectedCard"])) {
+    //     $selected_card = $_GET["SelectedCard"];
+    // }
+
+    // if(isset($_GET["SelectedLanguage"])) {
+    //     $selected_language = $_GET["SelectedLanguage"];
+    // }
+    
+    // header("Content-type: application/octet-stream");
+    // header("Content-Disposition: attachment; filename=".$selected_language.".strings");
+    // header("Pragma: no-cache");
+    // header("Expires: 0");
+    // // header('Content-Length: '.filesize('file.txt'));
+
+    // $search_query = "SELECT StringKeys, English, `$selected_language` FROM $selected_card WHERE `$selected_language` != '' ";
+
+    // $file = fopen('php://output', 'w');
+
+    // if($search_result = mysqli_query($connection, $search_query) and mysqli_num_rows($search_result) > 0) {
+    //     while($row = mysqli_fetch_assoc($search_result))
+    //     {
+    //         fwrite($file, "\"".$row['StringKeys']."\" = \"".$row[$selected_language]."\";\r\n");
+    //     }
+    // }
+
+    // fclose($file);
+    // exit();
 ?>
